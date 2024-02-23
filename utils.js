1e788408332bb068904dcbd10ebd0a1dad20b9cf
Vijay <94220135+vijay151096@users.noreply.github.com>
2024-02-23 12:49:35 +0530

    Ed25519 Basic Impl
    Signed-off-by: Vijay <94220135+vijay151096@users.noreply.github.com>

--- utils.js
+++ utils.js
@@ -1,7 +1,58 @@
 var fs = require('file-system');
+const {AssertionProofPurpose} = require("./lib/jsonld-signatures/purposes/AssertionProofPurpose");
+const {PublicKeyProofPurpose} = require("./lib/jsonld-signatures/purposes/PublicKeyProofPurpose");
+const {RsaSignature2018} = require("./lib/jsonld-signatures/suites/rsa2018/RsaSignature2018");
+const {Ed25519Signature2018} = require("./lib/jsonld-signatures/suites/ed255192018/Ed25519Signature2018");
 const readFile = () => {
     const filesRead = fs.readFileSync("vc.json");
     return JSON.parse(filesRead.toString());
 }
 
-module.exports = {readFile}
+const ProofType = {
+    ED25519: 'Ed25519Signature2018',
+    RSA: 'RsaSignature2018',
+};
+
+const ProofPurpose = {
+    Assertion: 'assertionMethod',
+    PublicKey: 'publicKey',
+};
+
+const getProofPurpose = (verifiableCredential) => {
+
+    let purpose ;
+    switch (verifiableCredential.proof.purpose) {
+        case ProofPurpose.Assertion:
+            purpose = new AssertionProofPurpose()
+            break;
+        case ProofPurpose.PublicKey:
+            purpose = new PublicKeyProofPurpose()
+            break;
+    }
+    return purpose;
+}
+
+const getSuite = (verifiableCredential) => {
+    let suite ;
+    switch (verifiableCredential.proof.type) {
+        case ProofType.ED25519: {
+            const suiteOptions = {
+                verificationMethod: verifiableCredential.proof.verificationMethod,
+                date: verifiableCredential.proof.created,
+            };
+            suite = new RsaSignature2018(suiteOptions);
+            break;
+        }
+        case ProofType.RSA: {
+            const suiteOptions = {
+                verificationMethod: verifiableCredential.proof.verificationMethod,
+                date: verifiableCredential.proof.created,
+            };
+            suite = new Ed25519Signature2018(suiteOptions);
+            break;
+        }
+    }
+    return suite;
+}
+
+module.exports = {readFile, getProofPurpose, getSuite}
--- verify.js
+++ verify.js
@@ -1,20 +1,14 @@
 const vcjs = require('@digitalcredentials/vc');
-const { RsaSignature2018 } = require('./lib/jsonld-signatures/suites/rsa2018/RsaSignature2018');
-const { AssertionProofPurpose } = require('./lib/jsonld-signatures/purposes/AssertionProofPurpose.js');
-const {readFile} = require("./utils");
+const {readFile, getProofPurpose, getSuite} = require("./utils");
 const jsonld = require("@digitalcredentials/jsonld");
 
 async function verifyCredential(){
   console.log("\n\n  ************ Verification Initiated ************ \n")
 
   const verifiableCredential = readFile();
-  const suiteOptions = {
-    verificationMethod: verifiableCredential.proof.verificationMethod,
-    date: verifiableCredential.proof.created,
-  };
+  const purpose = getProofPurpose(verifiableCredential);
+  const suite  = getSuite(verifiableCredential);
 
-  const purpose = new AssertionProofPurpose();
-  const suite  = new RsaSignature2018(suiteOptions);
   const vcjsOptions = {
     purpose,
     suite,
@@ -24,7 +18,7 @@ async function verifyCredential(){
 
   const result = await vcjs.verifyCredential(vcjsOptions);
 
-  // console.log("\nVerify Credential Response => ", JSON.stringify(result, null, 4));
+  console.log("\nVerify Credential Response => ", JSON.stringify(result, null, 4));
   console.log("\n  Verification Result => ", result.verified);
   console.log("\n\n  ************ Verification Completed ************ \n\n")
 }
