ff655553ff57a67074dc933e50e0b265939f570e
Vijay <94220135+vijay151096@users.noreply.github.com>
2024-04-03 09:26:47 +0530

    Refactor the code
    Signed-off-by: Vijay <94220135+vijay151096@users.noreply.github.com>

--- verify.js
+++ verify.js
@@ -2,7 +2,7 @@ const vcjs = require('@digitalcredentials/vc');
 const {readFile, getProofPurpose, getSuite, documentLoader} = require("./utils");
 const jsonld = require("@digitalcredentials/jsonld");
 
-async function verifyCredential(){
+async function verify(){
   console.log("\n\n  ************ Verification Initiated ************ \n")
   let vcFilePath = "VC/vc.json";
   if(process.argv.length > 1){
@@ -10,6 +10,19 @@ async function verifyCredential(){
   }
 
   const verifiableCredential = readFile(vcFilePath);
+  const result =  await verifyCredential(verifiableCredential);
+
+  if(!result.verified){
+    throw new Error("Verification Failed");
+  }
+
+  console.log("\nVerify Credential Response => ", JSON.stringify(result, null, 4));
+
+  console.log("\n\n  ************ Verification Completed ************ \n\n")
+}
+
+async function verifyCredential(verifiableCredential){
+
   const purpose = getProofPurpose(verifiableCredential);
   const suite  = await getSuite(verifiableCredential);
 
@@ -20,17 +33,10 @@ async function verifyCredential(){
     documentLoader: await documentLoader,
   };
 
-  const result = await vcjs.verifyCredential(vcjsOptions);
+  return await vcjs.verifyCredential(vcjsOptions);
 
-  console.log("\nVerify Credential Response => ", JSON.stringify(result, null, 4));
-  console.log("\n  Verification Result => ", result.verified);
-
-  if(!result.verified){
-    throw new Error("Verification Failed");
-  }
-  console.log("\n\n  ************ Verification Completed ************ \n\n")
 }
 
 
-module.exports = {verifyCredential}
+module.exports = {verifyCredential, verify}
 
