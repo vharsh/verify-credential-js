22cc4289e5d6bc3345c0663b36dde67619c636d7
Vijay <94220135+vijay151096@users.noreply.github.com>
2024-04-03 11:52:18 +0530

    INJI related Changes
    Signed-off-by: Vijay <94220135+vijay151096@users.noreply.github.com>

--- verify.js
+++ verify.js
@@ -1,25 +1,5 @@
 const vcjs = require('@digitalcredentials/vc');
 const {getProofPurpose, getSuite, documentLoader} = require("./utils");
-const jsonld = require("@digitalcredentials/jsonld");
-
-// async function verify(){
-//   console.log("\n\n  ************ Verification Initiated ************ \n")
-//   let vcFilePath = "VC/vc.json";
-//   if(process.argv.length > 1){
-//     vcFilePath = process.argv[1];
-//   }
-//
-//   const verifiableCredential = readFile(vcFilePath);
-//   const result =  await verifyCredential(verifiableCredential);
-//
-//   if(!result.verified){
-//     throw new Error("Verification Failed");
-//   }
-//
-//   console.log("\nVerify Credential Response => ", JSON.stringify(result, null, 4));
-//
-//   console.log("\n\n  ************ Verification Completed ************ \n\n")
-// }
 
 async function verifyCredential(verifiableCredential){
 
